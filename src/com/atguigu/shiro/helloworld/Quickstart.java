1594a17dad39a3b7617c7aea94559e0753ef49ce
lxj18235187311 <18235187311@163.com>
2016-09-18 21:24:23 +0800

    1
    1

--- src/com/atguigu/shiro/helloworld/Quickstart.java
+++ src/com/atguigu/shiro/helloworld/Quickstart.java
@@ -22,6 +22,7 @@ public class Quickstart {
     public static void main(String[] args) {
     	System.out.println("1111111111111111111111");
     	System.out.println("0000000000000000000000");
+    	System.out.println("update01");
         // The easiest way to create a Shiro SecurityManager with configured
         // realms, users, roles and permissions is to use the simple INI config.
         // We'll do that by using a factory that can ingest a .ini file and
